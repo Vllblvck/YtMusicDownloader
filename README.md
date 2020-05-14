665f63b0d2a03d8d8df38e681839f999119e0c81
vllblvck <vllblvck1998@gmail.com>
2020-05-14 21:17:47 +0200

    Fixed issues with pytube module

--- README.md
+++ README.md
@@ -1,7 +1,9 @@
-#YtMusicDownloader  
+# YtMusicDownloader  
 This is a simple python script that lets you download music from youtube using console  
-
-#Usage:  
-Install necessary python modules listed at the top of script  
+It downloads mp4 audio and converts it into mp3 file  
+You can download either single song or the whole playlist with it  
+It pretty slow and has some issues with pytube module but it works. Definitely needs improvement though  
+# Usage  
+Insall necessary python modules: pytube, ffmpeg   
 Change DOWNLOADS_PATH variable  
 Download music from youtube :) 
--- downloadytmusic.py
+++ downloadytmusic.py
@@ -5,11 +5,9 @@ try:
     from pytube import YouTube, Playlist
 except ImportError:
     print("You are lacking dependecies for the script")
-    print("Check README file for more instructions")
 
 
 DOWNLOADS_PATH = "/home/vllblvck/Music/"
-
 
 def mp3_to_mp4(songname_mp4, songname_mp3):
     print("Converting from mp4 to mp3...")
@@ -20,70 +18,94 @@ def mp3_to_mp4(songname_mp4, songname_mp3):
         .global_args('-loglevel', 'error')
         .run() 
     )
+    print("Removing mp4 file...")
     os.remove(DOWNLOADS_PATH + songname_mp4)
+    print("Done")
 
 
-def download_song(link):
-    print("Connecting to youtube...")
+def download_song(song_url):
+    download_complete = False
+    while not download_complete: 
+        try:
+            print("Trying to download: " + song_url)
+            song = YouTube(song_url)
+            stream = song.streams.filter(
+                adaptive=True,
+                mime_type="audio/mp4"
+            ).first()
     
-    try:
-        yt = YouTube(link)
-    except KeyError:
-        print("Provided link seems to be bad.")
-        print("Make sure it leads to a video")
-        sys.exit()
+            songname_mp4 = stream.default_filename
+            songname_mp3 = songname_mp4.replace(".mp4", ".mp3")
+            if os.path.exists(DOWNLOADS_PATH + songname_mp3):
+                print("Ommitting becaue it exists in given directory")
+                return
 
-    stream = yt.streams.filter(
-        adaptive=True,
-        mime_type="audio/mp4"
-    ).first()
-    
-    songname_mp4 = stream.default_filename
-    songname_mp3 = songname_mp4.replace(".mp4", ".mp3")
-    if os.path.exists(DOWNLOADS_PATH + songname_mp3):
-        print("Song already exists in given directory")
-        return
+            print("Downloading:" + songname_mp4)
+            stream.download(DOWNLOADS_PATH)
+            mp3_to_mp4(songname_mp4, songname_mp3)
+            download_complete = True
 
-    print("Downloading:" + songname_mp4)
-    stream.download(DOWNLOADS_PATH)
-    mp3_to_mp4(songname_mp4, songname_mp3)
-
-
-def download_playlist(link):
-    print("Connecting to youtube...")
-
-    try:
-        playlist = Playlist(link)
-    except KeyError:
-        print("Provided link seems to be bad")
-        print("Make sure it leads to a playlist")
-        sys.exit()
-    
-    for video in playlist.videos:
-        stream = video.streams.filter(
-            adaptive=True,
-            mime_type="audio/mp4"
-        ).first()
-
-        songname_mp4 = stream.default_filename
-        songname_mp3 = songname_mp4.replace(".mp4", ".mp3")
-        if os.path.exists(DOWNLOADS_PATH + songname_mp3):
-            print("Ommitting " + songname_mp4)
+        except KeyError:
+            print("Something went wrong because pytube is buggy :(")
+            print("Redoing the operation...")
             continue
+
+
+def download_playlist(playlist_url):
+    print("Gathering urls from playlist...")
+    playlist = Playlist(playlist_url)
+    song_urls = playlist.video_urls
+    download_complete = False
+    curr_idx = 0
+
+    while not download_complete: 
+        try:
+            for idx, song_url in enumerate(song_urls[curr_idx:], start=curr_idx):
+                print("[" + str(curr_idx) + "]Trying to download: " + song_url)
+                song = YouTube(song_url)
+                stream = song.streams.filter(
+                    adaptive=True,
+                    mime_type="audio/mp4"
+                ).first()
+                            
+                songname_mp4 = stream.default_filename
+                songname_mp3 = songname_mp4.replace(".mp4", ".mp3")
+                if os.path.exists(DOWNLOADS_PATH + songname_mp3):
+                    print("Ommitting becaue it exists in given directory")
+                    curr_idx = idx + 1
+                    continue
         
-        print("Downloading:" + songname_mp4)
-        stream.download(DOWNLOADS_PATH)
-        mp3_to_mp4(songname_mp4, songname_mp3)
+                print("Downloading:" + songname_mp4)
+                stream.download(DOWNLOADS_PATH)
+                mp3_to_mp4(songname_mp4, songname_mp3)
+                curr_idx = idx + 1
+
+            download_complete = True
+
+        except KeyError:
+            print("Something went wrong because pytube is buggy :(")
+            print("Redoing the operation...")
+            curr_idx = idx
+            continue
 
 
-if sys.argv[1] == "v" and sys.argv[2] is not None:
-    link = sys.argv[2]
-    download_song(link)
-elif sys.argv[1] == "p" and sys.argv[2] is not None:
-    link = sys.argv[2]
-    download_playlist(link)
-else:
-    print("You need to provide link and download type")
-    print("Correct syntax is:")
-    print("{v for video download or p for playlist download} {video/playlist link}")
-    sys.exit()
+def main():
+    args = sys.argv
+    args_len = len(args)
+    if (args_len == 3):
+        if args[1] == 'v':
+            download_song(args[2])
+        elif args[1] == 'p':
+            download_playlist(args[2])
+        else:
+            print("Correct syntax is:")
+            print("{v for video download or p for playlist download} {video/playlist url}")
+            sys.exit()
+    else:
+        print("Correct syntax is:")
+        print("{v for video download or p for playlist download} {video/playlist url}")
+        sys.exit()
+
+
+if __name__ == "__main__":
+    main()
