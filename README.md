0ab16644ee3e923a8df4af5bb899b303270a4a9f
vllblvck <vllblvck1998@gmail.com>
2020-05-24 02:23:30 +0200

    Improved arguments parsing and exception handling

--- README.md
+++ README.md
@@ -2,8 +2,8 @@
 This is a simple python script that lets you download music from youtube using console  
 It downloads mp4 audio and converts it into mp3 file  
 You can download either single song or the whole playlist with it  
-It's pretty slow and has some issues with pytube module but it works. Definitely needs improvement though  
+It's pretty slow and has some issues with pytube module but it works :)  
 # Usage  
 Insall necessary python modules: pytube, ffmpeg   
-Change DOWNLOADS_PATH variable  
+Change DOWNLOADS_PATH variable at the top of the script 
 Download music from youtube :) 
--- downloadytmusic.py
+++ downloadytmusic.py
@@ -1,111 +1,129 @@
-try:
-    import sys
-    import os
-    import ffmpeg
-    from pytube import YouTube, Playlist
-except ImportError:
-    print("You are lacking dependecies for the script")
+import argparse
+import os
+import sys
+import ffmpeg
+from pytube import YouTube, Playlist
+from pytube.exceptions import RegexMatchError
 
 
-DOWNLOADS_PATH = "path/to/music/folder"
+DOWNLOADS_PATH = '/path/to/music/directory/'
 
-def mp3_to_mp4(songname_mp4, songname_mp3):
-    print("Converting from mp4 to mp3...")
-    (
-        ffmpeg
-        .input(DOWNLOADS_PATH + songname_mp4)
-        .output(DOWNLOADS_PATH + songname_mp3)
-        .global_args('-loglevel', 'error')
-        .run() 
-    )
-    print("Removing mp4 file...")
-    os.remove(DOWNLOADS_PATH + songname_mp4)
-    print("Done")
-
-
-def download_song(song_url):
-    download_complete = False
-    while not download_complete: 
-        try:
-            print("Trying to download: " + song_url)
-            song = YouTube(song_url)
-            stream = song.streams.filter(
-                adaptive=True,
-                mime_type="audio/mp4"
-            ).first()
+class YtMusicDownloader:
     
-            songname_mp4 = stream.default_filename
-            songname_mp3 = songname_mp4.replace(".mp4", ".mp3")
-            if os.path.exists(DOWNLOADS_PATH + songname_mp3):
-                print("Ommitting becaue it exists in given directory")
-                return
-
-            print("Downloading:" + songname_mp4)
-            stream.download(DOWNLOADS_PATH)
-            mp3_to_mp4(songname_mp4, songname_mp3)
-            download_complete = True
-
-        except KeyError:
-            print("Something went wrong because pytube is buggy :(")
-            print("Redoing the operation...")
-            continue
+    def __init__(self, downloads_path):
+        self.downloads_path = downloads_path
 
 
-def download_playlist(playlist_url):
-    print("Gathering urls from playlist...")
-    playlist = Playlist(playlist_url)
-    song_urls = playlist.video_urls
-    download_complete = False
-    curr_idx = 0
-
-    while not download_complete: 
+    def mp3_to_mp4(self, songname_mp4, songname_mp3):
+        print('Converting from mp4 to mp3...')
         try:
-            for idx, song_url in enumerate(song_urls[curr_idx:], start=curr_idx):
-                print("[" + str(curr_idx) + "]Trying to download: " + song_url)
+            (
+                ffmpeg
+                .input(self.downloads_path + songname_mp4)
+                .output(self.downloads_path + songname_mp3)
+                .global_args('-loglevel', 'error')
+                .run() 
+            )
+            print('Removing mp4 file...')
+            os.remove(self.downloads_path + songname_mp4)
+            print('Done')
+        except ffmpeg.Error as e:
+            print('There was an error during file conversion')
+            print(e)
+            sys.exit()
+
+
+    def download_song(self, song_url):
+        download_complete = False
+        while not download_complete: 
+            try:
+                print('Trying to download: ' + song_url)
                 song = YouTube(song_url)
                 stream = song.streams.filter(
                     adaptive=True,
-                    mime_type="audio/mp4"
+                    mime_type='audio/mp4'
                 ).first()
-                            
+    
                 songname_mp4 = stream.default_filename
-                songname_mp3 = songname_mp4.replace(".mp4", ".mp3")
-                if os.path.exists(DOWNLOADS_PATH + songname_mp3):
-                    print("Ommitting becaue it exists in given directory")
-                    curr_idx = idx + 1
-                    continue
+                songname_mp3 = songname_mp4.replace('.mp4', '.mp3')
+                if os.path.exists(self.downloads_path + songname_mp3):
+                    print('Ommitting becaue song exists in given directory')
+                    return
+
+                print('Downloading:' + songname_mp4)
+                stream.download(self.downloads_path)
+                self.mp3_to_mp4(songname_mp4, songname_mp3)
+                download_complete = True
+
+            except KeyError:
+                print('Something went wrong with pytube module :(')
+                print('Redoing the operation...')
+                continue
+            except RegexMatchError:
+                print('Provided url is wrong')
+                sys.exit()
+
+
+    def download_playlist(self, playlist_url):
+        print('Gathering urls from playlist...')
+        playlist = Playlist(playlist_url)
+        song_urls = playlist.video_urls
+        download_complete = False
+        curr_idx = 0
+
+        while not download_complete: 
+            try:
+                for idx, song_url in enumerate(song_urls[curr_idx:], start=curr_idx):
+                    print('[' + str(curr_idx) + ']Trying to download: ' + song_url)
+                    song = YouTube(song_url)
+                    stream = song.streams.filter(
+                        adaptive=True,
+                        mime_type='audio/mp4'
+                    ).first()
+                            
+                    songname_mp4 = stream.default_filename
+                    songname_mp3 = songname_mp4.replace('.mp4', '.mp3')
+                    if os.path.exists(self.downloads_path + songname_mp3):
+                        print('Ommitting becaue song exists in given directory')
+                        curr_idx = idx + 1
+                        continue
         
-                print("Downloading:" + songname_mp4)
-                stream.download(DOWNLOADS_PATH)
-                mp3_to_mp4(songname_mp4, songname_mp3)
-                curr_idx = idx + 1
+                    print('Downloading:' + songname_mp4)
+                    stream.download(self.downloads_path)
+                    self.mp3_to_mp4(songname_mp4, songname_mp3)
+                    curr_idx = idx + 1
 
-            download_complete = True
+                download_complete = True
 
-        except KeyError:
-            print("Something went wrong because pytube is buggy :(")
-            print("Redoing the operation...")
-            curr_idx = idx
-            continue
+            except KeyError:
+                print('Something went wrong with pytube module :(')
+                print('Redoing the operation...')
+                curr_idx = idx
+                continue
+            except RegexMatchError:
+                print('Provided url is wrong')
+                sys.exit()
 
 
 def main():
-    args = sys.argv
-    args_len = len(args)
-    if (args_len == 3):
-        if args[1] == 'v':
-            download_song(args[2])
-        elif args[1] == 'p':
-            download_playlist(args[2])
-        else:
-            print("Correct syntax is:")
-            print("{v for video download or p for playlist download} {video/playlist url}")
-            sys.exit()
-    else:
-        print("Correct syntax is:")
-        print("{v for video download or p for playlist download} {video/playlist url}")
-        sys.exit()
+    parser = argparse.ArgumentParser()
+    parser.add_argument(
+        'download_type',
+        choices=['song', 'playlist']
+    )
+    parser.add_argument(
+        'url',
+        help='Url to song or playlist'
+    )
+    args = parser.parse_args()
 
+    yt = YtMusicDownloader(DOWNLOADS_PATH)
+    
+    if args.download_type == 'song':
+        yt.download_song(args.url)
+    elif args.download_type == 'playlist':
+        yt.download_playlist(args.url)
+     
 
-if __name__ == "__main__":
+if __name__ == '__main__':
     main()
